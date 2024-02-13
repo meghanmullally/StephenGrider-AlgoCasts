5ecc15325904d426d58cc1d48e8a70d97f689469
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-13 12:57:54 -0800

    learning the 3 possible solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,19 +7,37 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    // SOLUTION #2:
+
+    // SOLUTION #3
+    // reduce to condense down to 1 single string value
+    // reduce takes 2 arguments 
+    return str.split('').reduce((rev, char) =>   char + rev, '');
+        // function runs 1 time for every element
+
+        // instead of 1 liner 
+        // return str.split('').reduce((rev, char) =>   {return char + rev, ''});
 
 
 }
 
 module.exports = reverse;
 
-    // SOLUTION #1 
-    // const arr = str.split('');
-    // flip / reserve all the elements in the str
-    // arr.reverse();
-    // join elements back into str
-    // return arr.join('');
+// SOLUTION #1 
+// const arr = str.split('');
+// flip / reserve all the elements in the str
+// arr.reverse();
+// join elements back into str
+// return arr.join('');
 
-    // if you want to do a 1 liner
-    // return str.split('').reverse().join('');
+// if you want to do a 1 liner
+// return str.split('').reverse().join('');
+
+// SOLUTION #2:
+// let reversed = '';
+
+// // for, var declaration and iterating through each str 
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
