14e208b0e5bedf52f00d1967f179028868021a7c
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-13 12:36:19 -0800

    learning the 1st solution for reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // SOLUTION #2:
+
+
+}
 
 module.exports = reverse;
+
+    // SOLUTION #1 
+    // const arr = str.split('');
+    // flip / reserve all the elements in the str
+    // arr.reverse();
+    // join elements back into str
+    // return arr.join('');
+
+    // if you want to do a 1 liner
+    // return str.split('').reverse().join('');
