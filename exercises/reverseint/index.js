a376b97f287941f6f7c5a6283ec8eb8052c18b75
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-13 19:54:56 -0800

    learning about reversing and Int and using Math.sign for negative numbers

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+// working with a string instead of a number now 
+const reversed = n.toString().split('').reverse().join('');
+
+// if n < 0, the result will be -1 so use Math.sign to solve for the negative numbers
+return parseInt(reversed) * Math.sign(n);      
+
+}
 
 module.exports = reverseInt;
+
+
+// my first attempt
+// const numStr = n.toString().split('');
+
+// const reversed = parseInt(numStr.reverse().join(''));
+
+// return reversed;
