78706e80423f37e7877f7353129df4130c140d2f
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-13 20:49:09 -0800

    for of, for in

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+
+    }
+
+    // itterate through an object by using in
+    for ( let char in charMap) {
+        if( charMap[char] > max ) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
+
+// my first attempt
+
+
+// const chars = {};
+
+// for (let char of str) {
+//     if (!chars[char]) {
+//         chars[char] = 1
+//     } else {
+//         chars[char]++;
+//     }
+
+// }
+// return Math.max(chars.length);
