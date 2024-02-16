e678e89fb09e34a2d3b2d2ce25650ec6f1244d0b
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-15 21:15:16 -0800

    for loop and if statement solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,45 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+
+    for (let i = 1; i <= n; i++) {
+        //  check if number is multiple of 3 and 5? 
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        // is the number a multipe of 3?
+        } else if (i % 3 === 0 ) {
+            console.log('fizz');
+        // is the number a multipe of 5?
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+
+}
 
 module.exports = fizzBuzz;
+
+// my first attempts 
+// modulo operator 
+// if (n % 3 === 0){
+//     console.log("fizz")
+// } else if (n % 5 === 0) {
+//     console.log("buzz")
+// } else if (n % 3 === 0 && n % 5 === 0) {
+//     console.log("fizzbuzz")
+// } else {
+//     console.log(n)
+// }
+
+// if (n % 3 !== 0 || n % 5 !== 0 ){
+//     console.log(n)
+// } else if (n % 3 === 0){
+//     console.log("fizz")
+// } else if (n % 5 === 0) {
+//     console.log("buzz")
+// } else if (n % 3 === 0 && n % 5 === 0) {
+//     console.log("fizzbuzz")
+// }
