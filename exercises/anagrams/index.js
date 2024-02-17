797125612decf59e799293c2b51c34f70c93f6bf
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-16 19:05:09 -0800

    practice chunk and anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,64 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    // solution 2:
+    // compare the two - if absolutely identical they are an annagram
+return cleanString(stringA) === cleanString(stringB);
+
+
+}
+
+function cleanString(str) {
+    // first call replace, split, sort then join
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+}
+
 
 module.exports = anagrams;
+
+
+// my attempt 
+// const newA = stringA.replace(/[^\w]/g, '').toLowerCase();
+
+// const newB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+// if (newA.length === newB.length) {
+//     return true
+// } else {
+//     return false
+// }
+
+
+
+// SOLUTION 1:
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false;
+//     }
+
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false
+//         }
+//     }
+
+//     return true;
+
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {};
+
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -1,5 +1,6 @@
 // --- Directions
-// Given an array and chunk size, divide the array into many subarrays
+// Given an array and chunk size, divide the array 
+// into many subarrays
 // where each subarray is of length size
 // --- Examples
 // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
@@ -8,6 +9,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // solution 2 
+const chunked = [];
+let index =  0;
+
+while (index < array.length ) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+}
+
+return chunked;
+}
 
 module.exports = chunk;
+
+    // solution 1
+    // const chunked = [];
+
+    // for (let element of array) {
+    //     const last = chunked[chunked.length - 1];
+
+    //     if (!last || last.length === size) {
+    //         // pushing a element into
+    //         chunked.push([element]);
+    //     } else {
+    //         last.push(element);
+    //     }
+
+    // }
+    // return chunked;
